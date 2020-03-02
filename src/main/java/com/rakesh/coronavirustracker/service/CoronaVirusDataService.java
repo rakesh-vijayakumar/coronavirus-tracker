825014cc04698c5830e46c127d0a385369873cfa
Rakesh Vijayakumar <rakeshvit10@gmail.com>
2020-03-01 22:33:56 -0600

    initial commit

---
+++ src/main/java/com/rakesh/coronavirustracker/service/CoronaVirusDataService.java
@@ -0,0 +1,50 @@
+package com.rakesh.coronavirustracker.service;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.rakesh.coronavirustracker.model.LocationStats;
+
+@Service
+public class CoronaVirusDataService {
+
+	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+	private List<LocationStats> allStats = new ArrayList<>();
+
+	public List<LocationStats> getAllStats() {
+		return allStats;
+	}
+
+	@PostConstruct
+	@Scheduled(cron = "* * 1 * * *")
+	public void fetchVirusData() throws IOException, InterruptedException {
+		List<LocationStats> newStats = new ArrayList<>();
+		RestTemplate restTemplate = new RestTemplate();
+		String virusData = restTemplate.getForObject(VIRUS_DATA_URL, String.class);
+		StringReader csvBodyReader = new StringReader(virusData);
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+		for (CSVRecord record : records) {
+			LocationStats locationStat = new LocationStats();
+			locationStat.setState(record.get("Province/State"));
+			locationStat.setCountry(record.get("Country/Region"));
+			int latestCases = Integer.parseInt(record.get(record.size() - 1));
+			int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+			locationStat.setLatestTotalCases(latestCases);
+			locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+			newStats.add(locationStat);
+		}
+		this.allStats = newStats;
+	}
+
+}
